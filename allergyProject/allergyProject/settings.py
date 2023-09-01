cd5b92ad12637a5580f4e16f959f3bcd09324ae9
Kimdaehwan <etukdh6433@naver.com>
2023-09-01 14:13:45 +0900

    Change static_url

--- allergyProject/allergyProject/settings.py
+++ allergyProject/allergyProject/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',                                # 베이스 디렉도리에 있는 static 파일 쓴다.
