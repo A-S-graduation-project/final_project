681a729325962f4448a907ce45717590e68b78b6
SangJun <grw05177@naver.com>
2023-09-24 03:22:40 +0900

    views.py 함수에 따른 url 설정

--- allergyProject/board/urls.py
+++ allergyProject/board/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import BoardView, DetailView, BoardCreateView
+from .views import BoardView
+from . import views
 
 app_name = 'board'
 
 urlpatterns = [
     path('', BoardView.as_view(), name='board_list'),
-    path('board_create/', BoardCreateView.as_view(), name='board_create'),
-    path('board_detail/<int:pk>', DetailView.as_view(), name='board_detail'),
+    path('board_create/', views.create_board, name='board_create'),
+    path('board_detail/<int:bno>', views.read_board, name="board_detail"),
 ]
-
