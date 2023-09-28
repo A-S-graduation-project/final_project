cf04529e91554a8bb6bc57b1ec36d5e8a7fe4e31
parkswon1 <parkswon1@naver.com>
2023-09-28 18:17:23 +0900

    bookmark 모델 추가

--- allergyProject/signapp/apps.py
+++ allergyProject/signapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SignappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'signapp'
+
+    def ready(self):
+        import signapp.models_bookmark
