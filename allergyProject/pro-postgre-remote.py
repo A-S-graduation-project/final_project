d4b79f67ef3e583cdf681670fcd0d1518197d487
Kimdaehwan <etukdh6433@naver.com>
2023-10-12 20:26:11 +0900

    Add allergy translation algorithm

--- allergyProject/pro-postgre-remote.py
+++ allergyProject/pro-postgre-remote.py
@@ -7,12 +7,14 @@ def Processed(dict):
     rawmtrl = re.sub(r"[^\uAC00-\uD7A3a-zA-Z]", " ", dict['rawmtrl'])
     rawmtrl = rawmtrl.replace('•, \n', ' ')
 
+    allergy_list = ','.join(allergy_trans(dict['allergy']))
+
     try:
         procData = (dict['prdlstReportNo'],
                     dict['prdlstNm'],
                     dict['prdkind'],
                     rawmtrl,
-                    dict['allergy'],
+                    allergy_list,
                     dict['imgurl1'],
                     dict['manufacture'])
     except KeyError:
@@ -20,7 +22,37 @@ def Processed(dict):
 
     return procData
 
-allergy_list = []
+
+def allergy_trans(allergy):
+    allergy_list = []
+    for al in allergy.split(','):
+                    translation = {
+                        "대두를사용한제품과같은제조시설에서제조하고있습니다." : None,
+                        "이제품은우유" : None,
+                        "[d-토코페롤혼합형]" : None,
+                        "해당사항없음" : None,
+                        "알수 없음" : None,
+                        "일수없음" : None,
+                        "알수없음" : None,
+                        "없음" : None,
+                        "함유" : None,
+                        "함류" : None,
+                        "식품" : None,
+                        "(" : None,
+                        ")" : None,
+                        "●" : None,
+                        "*" : None,
+                    }
+                    translated = custom_make_translation(al, translation).strip()
+                    if translated not in allergy_list and translated:
+                        allergy_list.append(translated)
+    return allergy_list
+
+
+def custom_make_translation(text, translation):
+    regex = re.compile('|'.join(map(re.escape, translation)))
+    return regex.sub(lambda match: translation[match.group(0)], text)
+
 
 # DB 연결 #
 conn = psycopg2.connect(host='localhost',user='postgres',password='2017018023',dbname='allergydb',connect_timeout=32768)
@@ -72,10 +104,6 @@ while True:
         # print(procData)
 
         if procData:
-            for al in procData[4].split(','):
-                if al not in allergy_list and al != "없음" and al != "해당없음" and len(al) <= 6:
-                    allergy_list.append(al)
-
             # "" 없으면 소문자로 인식 #
             sql = """INSERT INTO products("prdlstReportNo", "prdlstNm", prdkind, rawmtrl, allergy, image, manufacture)"""\
                 """VALUES(%s, %s, %s, %s, %s, %s, %s)"""\
@@ -94,9 +122,7 @@ while True:
 
 conn.close()
 
-print(allergy_list)
-
 print("\nfood_sim.py 실행")
 print("========================================================================================")
-os.system("python ./food_sim.py")
+os.system("python ./allergyProject/food_sim.py")
 print("========================================================================================")
