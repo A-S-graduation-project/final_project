7b98853aa5c0e97789b3d3250d10a0b92434f040
parkseokwon <parkswon1@naver.com>
2023-10-13 23:18:03 +0900

    UI 보다 깔끔하게 바꿨고 게시판이 정상적으로 찜목록에 들어가는거 확인

--- allergyProject/board/view_add_board.py
+++ allergyProject/board/view_add_board.py
@@ -1,33 +1,31 @@
 from django.http import JsonResponse
-from django.urls import reverse
-from django.contrib import messages
 from django.views.decorators.http import require_POST
-from .models import Product
-from signapp.models_bookmark import FBookmark
+from .models import Board
+from signapp.models_bookmark import BBookmark
 from datetime import date
 
 @require_POST
 def like_button_view(request):
     # POST 요청에서 필요한 데이터를 가져옵니다.
-    fno = request.POST.get('fno')
+    get_bno = request.POST.get('bno')
     customer = request.user
 
-    product = Product.objects.get(prdlstReportNo=fno)
-    title = product.prdlstNm
+    board = Board.objects.get(bno=get_bno)
+    title = board.title
 
     # 현재 사용자와 제품에 대한 북마크가 이미 존재하는지 확인합니다.
-    existing_bookmark = FBookmark.objects.filter(CNO=customer, FNO=product).first()
+    existing_bookmark = BBookmark.objects.filter(CNO=customer, bNO=board).first()
 
     if existing_bookmark:
         # 북마크가 이미 존재하는 경우, 북마크를 삭제하여 해제합니다.
         existing_bookmark.delete()
-        return JsonResponse({'message': '찜해재했습니다.'})
+        return JsonResponse({'message': '북마크가 성공적으로 해제되었습니다.'})
     else:
         # 새로운 북마크를 생성합니다.
-        bookmark = FBookmark.objects.create(
+        bookmark = BBookmark.objects.create(
             TITLE=title,
-            FNO=product,
+            bNO=board,
             CNO=customer,
             CDATE=date.today()
         )
-        return JsonResponse({'message': '찜했습니다.'})
+        return JsonResponse({'message': '북마크가 성공적으로 추가되었습니다.'})
