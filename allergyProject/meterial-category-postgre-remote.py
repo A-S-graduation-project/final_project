56301d5699c34cddaf7b3f645522eba073653a05
SangJun <grw05177@naver.com>
2023-10-14 13:35:43 +0900

    각 카테고리를 DB에 넣는 파일

--- allergyProject/meterial-category-postgre-remote.py
+++ allergyProject/meterial-category-postgre-remote.py
@@ -11,7 +11,7 @@ cur.execute("""CREATE TABLE IF NOT EXISTS meterial_categories(
             id SERIAL PRIMARY KEY NOT NULL,
             materials varchar(50) UNIQUE NOT NULL)""")
 
-# 이후 빠진 allergy 추가 필요 #
+# 넣을 재료등 #
 meterials = ["소고기", "돼지고기", "닭고기", "육류", "채소류",
              "해물류", "달걀/유제품", "가공식품류", "쌀", "밀가루",
              "건어물류", "버섯류", "과일류", "콩/견과류", "곡류",
@@ -19,7 +19,7 @@ meterials = ["소고기", "돼지고기", "닭고기", "육류", "채소류",
 
 
 for meterial in meterials:
-    # allergy를 unique field이므로 존재하는 데이터의 경우 무시 #
+    # 재료를 meterials에 넣음 #
     try:
         sql = """INSERT INTO meterial_categories(id, meterials) VALUES(DEFAULT, '{0}')""".format(meterial)
         print(sql)
