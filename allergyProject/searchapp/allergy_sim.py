e141f9784dd2027e18fbf6a1de81a8802c8cf467
Kimdaehwan <etukdh6433@naver.com>
2023-10-20 15:28:44 +0900

    Modify Collarbor Algorithm

--- allergyProject/searchapp/allergy_sim.py
+++ allergyProject/searchapp/allergy_sim.py
@@ -1,5 +1,6 @@
 import pymysql, psycopg2
 import pandas as pd
+import numpy as np
 from math import sqrt
 
 # 유사도 계산 #
@@ -68,6 +69,7 @@ def getRecommendation (data, allergy, sim_function=sim_person):
 
     return li
 
+
 def food_recommend():
     # DB 연결 #
     conn = psycopg2.connect(host='localhost',
@@ -109,6 +111,32 @@ def food_recommend():
 
 #=========================================================================================================================================#
 
+def user_based_recommendation(user, user_similarity, user_item_matrix):
+    # Get the index of the active user
+    user_index = list(user_item_matrix.index).index(user)
+    # Find similar users and their similarity scores
+    similar_users = list(enumerate(user_similarity[user_index]))
+    # Sort the similar users by similarity score in descending order
+    similar_users = sorted(similar_users, key=lambda x: x[1], reverse=True)
+    # Initialize recommendations dictionary
+    recommendations = {}
+
+    # Iterate through similar users and their interactions
+    for user_id, similarity_score in similar_users:
+        if user_id == user_index:
+            continue  # Skip the active user
+        for item in user_item_matrix.columns:
+            if user_item_matrix.at[user, item] == 0 and user_item_matrix.at[user_id+1, item] > 0:
+                if item not in recommendations:
+                    recommendations[item] = 0
+                recommendations[item] += similarity_score * user_item_matrix.at[user_id+1, item]
+    
+    # Sort the recommendations by score in descending order
+    recommendations = dict(sorted(recommendations.items(), key=lambda x: x[1], reverse=True))
+    
+    return recommendations
+
+
 def board_recommend():
     # DB 연결 #
     conn = psycopg2.connect(host='localhost',
@@ -119,31 +147,49 @@ def board_recommend():
     cur = conn.cursor()
 
     # Mysql에서 DATA 읽기 (전처리 포함) #
+    cur.execute("""SELECT cno from customers""")
+    cnoData = cur.fetchall()
+    # print(cnoData[0:2]) 
+
+    # Mysql에서 DATA 읽기 (전처리 포함) #
     cur.execute("""SELECT bno from boards""")
-    boardData = cur.fetchall()
-    # print(proData[0:2])                                             # product data 확인용
+    bnoData = cur.fetchall()
+    # print(bnoData[0:2])
 
-    cur.execute("""SELECT "CNO_id", "bNO_id", allerinfo, 3 FROM bbookmark RIGHT OUTER JOIN customers ON customers.cno = bbookmark."CNO_id" """)
+    # Mysql에서 DATA 읽기 (전처리 포함) #
+    cur.execute("""SELECT "CNO_id", "bNO_id" FROM bbookmark RIGHT OUTER JOIN customers ON customers.cno = bbookmark."CNO_id" """)
     choData = cur.fetchall()
-    print(choData[0:2])                                             # user data 확인용
+    # print(choData[0:2])
 
-    # 형식 변환 #
-    pdBoardData = pd.DataFrame(boardData)
-    pdChoData = pd.DataFrame(choData)
+    # 기초 데이터 만들기 #
+    data = {'User':[cno[0] for cno in cnoData]}
 
-    # 병합 #
-    merge_data = pd.concat([pdBoardData, pdChoData], join='outer')
-    # print(merge_data)
+    for bno in bnoData:
+        data[bno[0]] = [0 for _ in cnoData]
 
-    # 데이터 분포 #
-    boardAlData = merge_data.pivot_table(3, index=1, columns=2)       # 4 : 'rating', 3 : 'prdlstReportNo', 2: 'allergy'
-    boardAlData.fillna(0, inplace=True)                               # NaN -> 0
-    print(boardAlData)
+    # bookmark data 적용 #
+    for cho in choData:
+        if cho[0] != None:
+            data[cho[1]][cho[0]-1] += 3
 
-    # 결과 #
-    # 알레르기 key가 userdata에 존재하지 않은 경우 오류 발생 #
-    re = getRecommendation(boardAlData, '2, 3')                       # userdata 수집 필요
-    print(re[:10])
+    # sample #
+    # data = {
+    # 'User': [1,2,3,4],
+    # 1: [5, 4, 0, 0],
+    # 2: [0, 0, 3, 4],
+    # 3: [2, 0, 0, 0],
+    # }
+
+    df = pd.DataFrame(data)
+    df.set_index('User', inplace=True)
+    print(df)
+    
+    user_similarity = np.dot(df, df.T) / (np.linalg.norm(df, axis=1)[:, np.newaxis] * np.linalg.norm(df.T, axis=0))
+
+    user = 1
+    user_item_matrix = df
+    recommend_items = user_based_recommendation(user, user_similarity, user_item_matrix)
+    print(recommend_items)
 
     conn.close()
 
