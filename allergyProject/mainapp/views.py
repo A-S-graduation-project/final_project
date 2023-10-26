c72d7f9e8e759b4dd5f808332411f4fdaa77517e
Kimdaehwan <etukdh6433@naver.com>
2023-10-26 18:39:13 +0900

    home 화면에 Collarbor 출력 완료

--- allergyProject/mainapp/views.py
+++ allergyProject/mainapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.db.models import Q
 
+from searchapp.models import Product
+try:
+    from searchapp.collarbor import food_recommend
+except:
+    pass
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -9,3 +15,22 @@ class HomeView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = 'mainapp/about.html'
+
+
+def Collarbor(request):
+    username = request.user
+    collarbor_list = []
+    collarbors = []
+    
+    if not username.is_anonymous:
+        collarbor_list = food_recommend(str(username)).keys()
+
+    for col in collarbor_list:
+        collarbor = Product.objects.all().get(
+            Q(prdlstReportNo__exact = col)
+        )
+        collarbors.append(collarbor)
+
+    # print(collarbors)
+
+    return render(request, 'mainapp/home.html', {'collarbors':collarbors})
