48f4a95f33d0fb9c0549ee5d025cdc6813f5dba9
Kimdaehwan <etukdh6433@naver.com>
2023-10-27 16:33:56 +0900

    북마크를 많이 한 상품 보여주기 추가

--- allergyProject/mainapp/views.py
+++ allergyProject/mainapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from searchapp.models import Product
+from board.models import Board
+
+from signapp.models_bookmark import FBookmark
+from signapp.models_bookmark import BBookmark
 try:
     from searchapp.collarbor import food_recommend
+    from searchapp.collarbor import board_recommend
 except:
     pass
 # Create your views here.
@@ -19,18 +24,45 @@ class AboutView(TemplateView):
 
 def Collarbor(request):
     username = request.user
-    collarbor_list = []
-    collarbors = []
+    fcollarbors = []
+    bcollarbors = []
+    ranks = []
     
     if not username.is_anonymous:
-        collarbor_list = food_recommend(str(username)).keys()
+        fcollarbor_list = food_recommend(str(username)).keys()
+        bcollarbor_list = board_recommend(str(username)).keys()
 
-    for col in collarbor_list:
-        collarbor = Product.objects.all().get(
-            Q(prdlstReportNo__exact = col)
-        )
-        collarbors.append(collarbor)
+        for col in fcollarbor_list:
+            collarbor = Product.objects.all().get(
+                Q(prdlstReportNo__exact = col)
+            )
+            fcollarbors.append(collarbor)
 
-    # print(collarbors)
+        for col in bcollarbor_list:
+            collarbor = Board.objects.all().get(
+                Q(bno__exact = col)
+            )
+            bcollarbors.append(collarbor)
+    else:
+        counts = {}
+        fblist = FBookmark.objects.values('FNO')
 
-    return render(request, 'mainapp/home.html', {'collarbors':collarbors})
+        for fno in fblist:
+            if fno['FNO'] in ranks:
+                counts[fno['FNO']] += 1
+            else:
+                counts[fno['FNO']] = 1
+
+        counts = sorted(counts.items(), reverse=True, key=lambda x:x[1])[:5]
+
+        for fno in counts:
+            rank_list = Product.objects.all().get(
+                Q(prdlstReportNo__exact = fno[0])
+            )
+            ranks.append(rank_list)
+
+    # print(fcollarbors)
+    # print(bcollarbors)
+    # print(ranks)
+
+    return render(request, 'mainapp/home.html', {'fcollarbors':fcollarbors, 'bcollarbors':bcollarbors, 'ranks':ranks})
